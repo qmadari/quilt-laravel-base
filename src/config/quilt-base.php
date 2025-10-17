d1302db0ab90e1e75e10f242718271ae89992d6a
Quinten Madari <quinten@bigfoot.psy.vu.nl>
2025-10-17 21:59:17 +0200

    moved app config functionality into quilt-base config

--- src/config/quilt-base.php
+++ src/config/quilt-base.php
@@ -1,20 +1,30 @@
 <?php
 
 return [
+    // some descriptives
+    'repo_url' => env('APP_REPO_URL','https://bitbucket.org'),
+    'api_version' => env('API_VERSION','0.0.0'),
+    'api_description' => env('API_DESCRIPTION','API Landing Page'),
+
     // skip registering the web root route (if true, points to the default laravel page instead of api landing page)
     'skip_api_landing_route_registration' => false,
 
+    // Enable parts of container entrypoint helper command
     'entrypoint_steps' => [
         'scribe-generate' => true,
         'seed-apidocstokenissuer' => true,
         'seed-frontendtokenissuer' => true,
     ],
 
-    // Custom commands with or without arguments
+    // Add more custom commands to the entrypoint helper if necessary (ones created in this specific project)
+    // with or without arguments
     'custom_entrypoint_commands' => [
-        // command only format e.g.
+        //// Examples:
+        
+        //// command only format e.g.
 	    //'cache:clear',
-        // commands with arguments e.g.
+        
+        //// commands with arguments e.g.
         // 'db:seed' => [
         //     '--class' => 'UserIdSeeder',
         //     '--force' => true,
