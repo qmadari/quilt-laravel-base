cff2b9de84a11687cefbacd728631b20a3edc7e4
Quinten Madari <quinten@bigfoot.psy.vu.nl>
2025-11-24 04:13:13 +0100

    added configs for token issuing

--- src/config/quilt-base.php
+++ src/config/quilt-base.php
@@ -8,7 +8,17 @@ return [
 
     // skip registering the web root route (if true, points to the default laravel page instead of api landing page)
     'skip_api_landing_route_registration' => false,
-
+    'skip_token_route_registration' => false,
+    
+    // Docs API secret validation
+    'protect_docs' => env('QUILT_PROTECT_DOCS', true),
+    'docs_api_secret' => env('API_SECRET_DOCS', '12345'),
+    
+    // Token issuer configuration
+    'token_issuer_user' => env('TOKEN_ISSUER_USER', 'FrontendTokenIssuer'),
+    'token_issuer_user_docs' => env('TOKEN_ISSUER_USER_DOCS', 'ApiDocsTokenIssuer'),
+    'token_lifetime_minutes' => env('TOKEN_LIFETIME_MINUTES', 15),
+    
     // Enable parts of container entrypoint helper command
     'entrypoint_steps' => [
         'scribe-generate' => true,
@@ -16,6 +26,7 @@ return [
         'seed-frontendtokenissuer' => true,
     ],
 
+    
     // Add more custom commands to the entrypoint helper if necessary (ones created in this specific project)
     // with or without arguments
     'custom_entrypoint_commands' => [
